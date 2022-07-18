05e518e2dc22aad3552bdd5d22e30d994568a79e
Rian Sousa <99686158+rian-sousa@users.noreply.github.com>
2022-07-18 15:43:44 -0300

    Project created

---
+++ src/main/java/com/photon/sb_jpa/SbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.photon.sb_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaApplication.class, args);
+	}
+
+}
