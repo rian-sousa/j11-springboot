10177e875ec74601f16d027295ff126ae7e90b1e
Rian Sousa <99686158+rian-sousa@users.noreply.github.com>
2022-07-25 10:20:02 -0300

    JsonIgnore getOrder

--- src/main/java/com/photon/sb_jpa/config/TestConfig.java
+++ src/main/java/com/photon/sb_jpa/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.photon.sb_jpa.entities.Category;
 import com.photon.sb_jpa.entities.Order;
+import com.photon.sb_jpa.entities.OrderItem;
 import com.photon.sb_jpa.entities.Product;
 import com.photon.sb_jpa.entities.Users;
 import com.photon.sb_jpa.entities.enums.OrderStatus;
 import com.photon.sb_jpa.repositories.CategoryRepository;
+import com.photon.sb_jpa.repositories.OrderItemRepository;
 import com.photon.sb_jpa.repositories.OrderRepository;
 import com.photon.sb_jpa.repositories.ProductRepository;
 import com.photon.sb_jpa.repositories.UsersRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
 
     @Override
@@ -69,6 +74,13 @@ public class TestConfig implements CommandLineRunner {
 
         usersRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
     }
     
 }
