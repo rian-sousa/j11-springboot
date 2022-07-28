fa1d78200529705ef882438778bbc498a4cc3cfa
Rian Sousa <99686158+rian-sousa@users.noreply.github.com>
2022-07-28 12:00:29 -0300

    delete Exception handling

---
+++ src/main/java/com/photon/sb_jpa/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.photon.sb_jpa.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+    
+}
